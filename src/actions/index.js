41ed6d42f24b7d6dcb4bd1f547b5aca538aad020
TJ <abongsjoel@gmail.com>
2020-12-26 22:53:02 +0100

    Fix users issue

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,7 +10,7 @@ export const fetchPosts = () => async dispatch => {
 };
 
 export const fetchUser = id => async dispatch => {
-  const { data } = await jsonplaceholder.get(`/user/${id}`);
+  const { data } = await jsonplaceholder.get(`/users/${id}`);
 
   dispatch ({
     type: 'FETCH_USER',
