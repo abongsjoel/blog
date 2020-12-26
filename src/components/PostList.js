e6dbca1059994d823fce4e39cf1a7df493202552
TJ <abongsjoel@gmail.com>
2020-12-26 13:11:26 +0100

    A touch more setup

---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class PostList extends Component {
+  render() {
+    return (
+      <div>
+        Post List
+      </div>
+    )
+  }
+};
+
+export default PostList;
