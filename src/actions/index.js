2f0e090316bde679e1eaa0f2e096a38eaf583a89
TJ <abongsjoel@gmail.com>
2020-12-27 00:20:30 +0100

    Memorization issues

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonplaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch => {
@@ -9,11 +10,22 @@ export const fetchPosts = () => async dispatch => {
   });
 };
 
-export const fetchUser = id => async dispatch => {
-  const { data } = await jsonplaceholder.get(`/users/${id}`);
+// export const fetchUser = id => async dispatch => {
+//   const { data } = await jsonplaceholder.get(`/users/${id}`);
 
-  dispatch ({
-    type: 'FETCH_USER',
-    payload: data
-  })
-}
+//   dispatch ({
+//     type: 'FETCH_USER',
+//     payload: data
+//   })
+// }
+
+export const fetchUser = function(id) { 
+  return _.memoize(async function(dispatch)  {
+    const { data } = await jsonplaceholder.get(`/users/${id}`);
+
+    dispatch ({
+      type: 'FETCH_USER',
+      payload: data
+    });
+  });
+};
--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,5 +1,3 @@
-import { act } from "react-dom/test-utils";
-
 export default (state = [], action) => {
   switch (action.type) {
     case 'FETCH_POSTS':
