bc55694a8d7eeba13fb4b61da02ee7135ec1e3e2
TJ <abongsjoel@gmail.com>
2020-12-26 21:05:29 +0100

    Switch statements in Reducers

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,3 +1,10 @@
-export default () => {
-  return 123;
+import { act } from "react-dom/test-utils";
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_POSTS':
+      return action.payload;
+    default:
+      return state;
+  }
 };
