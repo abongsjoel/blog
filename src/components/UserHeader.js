91346fe9876c82adde2f6960b2bb21bae75c4403
TJ <abongsjoel@gmail.com>
2020-12-26 23:10:17 +0100

    Find relevant users

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -6,16 +6,24 @@ export class UserHeader extends Component {
   componentDidMount() {
     this.props.fetchUser(this.props.userId);
   }
-  
+
   render() {
+    const user = this.props.users.find(user => user.id === this.props.userId);
+
+    if (!user) {
+      return null;
+    }
+
     return (
-      <div>
-        User Header
+      <div className="header">
+        {user.name}
       </div>
     )
   }
 };
 
+const mapStateToProps = state => {
+  return { users: state.users };
+}
 
-
-export default connect(null, { fetchUser })(UserHeader);
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
