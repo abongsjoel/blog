1f8d81a11153e0f48d1effd76956b989947a614a
TJ <abongsjoel@gmail.com>
2020-12-26 14:20:54 +0100

    Make a request from an action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
-export const fetchPosts = () => {
+import jsonplaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = async () => {
+  const response = await jsonplaceholder.get('/posts');
+
   return {
-    type: 'FETCH_POSTS'
+    type: 'FETCH_POSTS',
+    payload: response
   };
 };
