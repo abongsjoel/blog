32368bdbce0d487e7c55ea4f9f8f0b2cf48910b2
TJ <abongsjoel@gmail.com>
2020-12-27 01:16:18 +0100

    Find unique userIds

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
 
 export class UserHeader extends Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
-
   render() {
     const { user } = this.props;
 
@@ -26,4 +21,4 @@ const mapStateToProps = (state, ownProps) => {
   return { user: state.users.find(user => user.id === ownProps.userId) };
 }
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
